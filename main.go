1d30a09cf63ef4dabea8fb0ea83302cf08cb06fd
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-03-06 19:44:11 +0900

    テキストが描画できるようにした

--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
-    "log"
+	"image/color"
+	"log"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 )
 
 const (
 	screenWidth = 640
 	screenHeight = 480
+	fontSize = 30
 )
+
+var (
+	arcadeFont	font.Face
+)
+
+func init() {
+	tt, err := opentype.Parse(fonts.PressStart2P_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	const dpi = 72
+	arcadeFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    fontSize,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Mode int
 
@@ -55,10 +83,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	case ModeGameOver:
 		ebitenutil.DebugPrint(screen, "ModeGameOver")
 	}
+
+
+	text.Draw(screen, "DrawText", arcadeFont, 50, 50, color.White)
+ 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-    return 300, 300
+    return 320, 240
 }
 
 func main() {
