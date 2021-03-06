2de6e66db3fa1afce8447a8658c1f4e690425b64
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2021-03-06 17:52:49 +0900

    クリックで画面を変化するようにした

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module flappyGorira
 
 go 1.13
 
-require github.com/hajimehoshi/ebiten v1.12.9 // indirect
+require (
+	github.com/hajimehoshi/ebiten v1.12.9 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.0.6 // indirect
+)
--- main.go
+++ main.go
@@ -2,29 +2,69 @@ package main
 
 import (
     "log"
-
-    "github.com/hajimehoshi/ebiten"
-    "github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct{}
+const (
+	screenWidth = 640
+	screenHeight = 480
+)
 
-func (g *Game) Update(screen *ebiten.Image) error {
+type Mode int
+
+const (
+	ModeTitle Mode = iota
+	ModeGame
+	ModeGameOver
+)
+
+type Game struct{
+	mode Mode
+}
+
+func NewGame() *Game {
+	g := &Game{}
+	return g
+}
+
+func clickMouseButton() bool {
+	return inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft)
+}
+
+func (g *Game) Update() error {
+	switch g.mode{
+	case ModeTitle:
+		if clickMouseButton(){ g.mode = ModeGame }
+	case ModeGame:
+		if clickMouseButton(){ g.mode = ModeGameOver }
+	case ModeGameOver:
+		if clickMouseButton(){ g.mode = ModeTitle }	
+	}
+
     return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-    ebitenutil.DebugPrint(screen, "Ebiten")
+	switch g.mode{
+	case ModeTitle:
+		ebitenutil.DebugPrint(screen, "ModeTitle")
+	case ModeGame:
+		ebitenutil.DebugPrint(screen, "ModeGame")
+	case ModeGameOver:
+		ebitenutil.DebugPrint(screen, "ModeGameOver")
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-    return 120, 140
+    return 300, 300
 }
 
 func main() {
-    ebiten.SetWindowSize(500, 500)
-    ebiten.SetWindowTitle("Ebiten")
-    if err := ebiten.RunGame(&Game{}); err != nil {
+    ebiten.SetWindowSize(screenWidth, screenHeight)
+    ebiten.SetWindowTitle("FlappyGorira")
+    if err := ebiten.RunGame(NewGame()); err != nil {
         log.Fatal(err)
     }
 }
